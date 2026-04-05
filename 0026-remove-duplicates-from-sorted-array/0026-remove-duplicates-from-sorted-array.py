68b906e45be057a9f50991058487eeef4a95e6aa
Mahmoud Mamdouh <110698058+Mahmooo0od@users.noreply.github.com>
2026-04-05 19:04:28 +0200

    Time: 107 ms (5.16%), Space: 20.4 MB (87.21%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -5,10 +5,7 @@ class Solution:
         nums.sort()
         i=0
         while i<K : 
-            if i==0 : 
-                i+=1
-                continue
-            if nums[i]==nums[i-1]:
+            if nums[i]==nums[i-1] and i!=0:
                 nums.append(nums[i])
                 nums.pop(i) 
             else : 
