0d0592b01568dc3da3c08a3a66a71627fb92b40c
Mahmoud Mamdouh <110698058+Mahmooo0od@users.noreply.github.com>
2026-05-08 02:02:16 +0300

    Time: 513 ms (66.5%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -1,5 +1,5 @@
 select TO_CHAR(activity_date, 'YYYY-MM-DD') AS day , count(distinct user_id) as active_users
 from Activity 
-WHERE activity_date > TO_DATE('2019-07-27', 'YYYY-MM-DD') - 30 
-  AND activity_date <= TO_DATE('2019-07-27', 'YYYY-MM-DD')
+WHERE activity_date > DATE '2019-07-27' - 30 
+  AND activity_date <= DATE '2019-07-27'
 group by activity_date
