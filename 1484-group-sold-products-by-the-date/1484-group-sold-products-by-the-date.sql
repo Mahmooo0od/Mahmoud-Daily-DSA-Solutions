355045d73ee6b30ffd45d7d26bc8433af6c84312
Mahmoud Mamdouh <110698058+Mahmooo0od@users.noreply.github.com>
2026-05-09 16:31:59 +0300

    Time: 479 ms (75.94%), Space: 0B (100%) - LeetHub

---
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -0,0 +1,7 @@
+
+SELECT TO_CHAR(sell_date, 'YYYY-MM-DD') AS "sell_date", 
+COUNT(DISTINCT(product)) AS "num_sold",
+LISTAGG(product, ',') WITHIN GROUP (ORDER BY product) AS "products"
+FROM (select distinct * from Activities) 
+GROUP BY sell_date 
+ORDER BY sell_date;
