41b240c4ea76bf08ba063468317a6a22decd2c6d
Mahmoud Mamdouh <110698058+Mahmooo0od@users.noreply.github.com>
2026-05-09 19:26:33 +0300

    Time: 420 ms (73.72%), Space: 0B (100%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,7 +1,7 @@
-select e1.employee_id
+select e1.employee_id 
 from Employees e1
-where e1.salary<30000 and not exists(select e2.employee_id 
-                                     from employees e2
-                                     where e2.employee_id = e1.manager_id)
-and manager_id is not null
+where e1.salary<30000 and e1.manager_id not in(select e2.employee_id 
+                                               from employees e2
+                                               where e2.employee_id = e1.manager_id)
+and e1.manager_id is not null
 order by employee_id
