dbb20623767b5bf841f831723314063d46e5d1c5
Mahmoud Mamdouh <110698058+Mahmooo0od@users.noreply.github.com>
2026-05-25 10:01:41 +0300

    Time: 595 ms (19.05%), Space: 0B (100%) - LeetHub

---
+++ 1179-reformat-department-table/1179-reformat-department-table.sql
@@ -0,0 +1,17 @@
+select *
+from Department 
+pivot(sum(revenue)
+      FOR month IN (
+        'Jan' AS Jan_Revenue,
+        'Feb' AS Feb_Revenue,
+        'Mar' AS Mar_Revenue,
+        'Apr' AS Apr_Revenue,
+        'May' AS May_Revenue,
+        'Jun' AS Jun_Revenue,
+        'Jul' AS Jul_Revenue,
+        'Aug' AS Aug_Revenue,
+        'Sep' AS Sep_Revenue,
+        'Oct' AS Oct_Revenue,
+        'Nov' AS Nov_Revenue,
+        'Dec' AS Dec_Revenue
+    ) )
