93d77a9fd7e242062646e0cdbbecda5c73e05971
Mahmoud Mamdouh <110698058+Mahmooo0od@users.noreply.github.com>
2026-05-27 18:04:38 +0300

    Time: 528 ms (5.69%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,3 +1,8 @@
-SELECT MAX(salary) SecondHighestSalary FROM Employee 
-
-WHERE salary<(SELECT MAX(salary) FROM Employee);
+/* Write your PL/SQL query statement below */
+SELECT MAX(salary)  as SecondHighestSalary
+FROM (
+SELECT id,
+salary,
+DENSE_RANK() OVER(ORDER BY salary DESC) as rnk
+FROM employee)
+WHERE rnk = 2;
