0eb698f0319c643e8167b69378ed0dcc5aac58d9
Mahmoud Mamdouh <110698058+Mahmooo0od@users.noreply.github.com>
2026-05-27 21:22:15 +0300

    Time: 572 ms (39.33%), Space: 0B (100%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,7 @@
+select to_char(trans_date,'YYYY-MM') as month, country , count(state) as trans_count , 
+       count(case when state='approved' then 1 end) as approved_count,sum(amount) as trans_total_amount,
+       sum(case when state='approved' then amount else 0 end) as approved_total_amount
+       
+from Transactions 
+group by to_char(trans_date,'YYYY-MM') , country 
+order by month
