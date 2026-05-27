5a1f4236bc5f471b481d312d78350591fff135a3
Mahmoud Mamdouh <110698058+Mahmooo0od@users.noreply.github.com>
2026-05-28 01:14:14 +0300

    Time: 620 ms (92.14%), Space: 0B (100%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,16 @@
+with cte as (select *
+             from( select delivery_id ,
+                          customer_id ,
+                          order_date ,
+                          customer_pref_delivery_date ,
+                       Row_Number() over (partition by customer_id order by order_date ) as Rn
+
+              from Delivery )
+              where RN=1 )
+
+
+select round(count( distinct case when order_date = customer_pref_delivery_date then customer_id end ) /
+        count(distinct customer_id)*100,2) as immediate_percentage
+
+from cte 
+
