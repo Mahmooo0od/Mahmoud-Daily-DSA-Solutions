5280f28c43ad544d287eb84330b2f963503e1ba6
Mahmoud Mamdouh <110698058+Mahmooo0od@users.noreply.github.com>
2026-06-09 00:02:02 +0300

    Time: 392 ms (54.9%), Space: 0B (100%) - LeetHub

---
+++ 3220-odd-and-even-transactions/3220-odd-and-even-transactions.sql
@@ -0,0 +1,6 @@
+select to_char(transaction_date,'YYYY-MM-DD') TRANSACTION_DATE,
+sum(case when mod(amount,2)=1 then amount else 0 end) odd_sum,
+sum(case when mod(amount,2)=0 then amount else 0 end) even_sum
+from transactions
+group by transaction_date
+order by transaction_date
