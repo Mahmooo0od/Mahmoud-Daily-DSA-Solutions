51cdcd468c432e2808348676d38d9b48764afd43
Mahmoud Mamdouh <110698058+Mahmooo0od@users.noreply.github.com>
2026-08-12 08:10:33 +0300

    Time: 3 ms (32.95%), Space: 19.4 MB (24.71%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,21 +1,19 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        set_1=set()
-        c=0 
+        set_1=set() 
         New=0 
 
-        while 1==1 : 
-            if c>len(set_1): 
-                return False 
+        while True :  
             while n>0 : 
                 L=n%10 
                 n//=10 
-                New=New+pow(L,2) 
+                New=New+L**2 
             if New==1: 
                 return True 
             else : 
                 n=New 
+                if New in set_1: 
+                    return False
                 set_1.add(New)
-                c+=1
                 New=0
         
